6594027a8f7734ed3bd1df3d9db57faca4224d53
shaoming123 <greatfoodprefer@gmail.com>
2021-03-26 19:50:33 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h2>dsd</h2>
+  <Navbar />
   );
 }
 
